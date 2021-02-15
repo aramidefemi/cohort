116adcdd6fb5ea86ba47faa256f43772e05d84b7
Oluwafemi Olasbuomi <olasubomifemi98@gmail.com>
2021-02-15 23:01:45 +0100

    image uploads

--- src/pages/LandingPage/Components/NetworkSection.js
+++ src/pages/LandingPage/Components/NetworkSection.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ladybg from '../../../assets/images/ladybg.svg';
+import ladybg from '../../../assets/images/cloud.svg';
 
-const WhySection = () => {
+const NetworkSection = () => {
   return (
-    <div className="WhySection">
-      <div className="image">
-        <img src={ladybg} alt="" />
-      </div>
+    <div className="NetworkSection">
+
       <div className="text">
-        <h3>Why Hadiel?</h3>
+        <h2>Over 100+ Hospital Network</h2>
         <h4>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum diam orci pretium a pharetra, feugiat cursus. Dictumst risus, sem egestas odio cras adipiscing vulputate. Nisi, risus in suscipit non. Non commodo volutpat, pharetra, vel.
-        </h4>
-        <Link>About Us</Link>
+        </h4> 
+      </div>      
+      <div className="image">
+        <img src={ladybg} alt="" />
+        <button className="primary">Learn More</button>
       </div>
     </div>
   );
 };
 
-export default WhySection;
+export default NetworkSection;
