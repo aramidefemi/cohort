91f5d531a6adc5c70e7f94c6aae137aa8a4e7727
Oluwafemi Olasbuomi <olasubomifemi98@gmail.com>
2021-05-19 09:06:27 +0100

    :okay_hand:

--- src/pages/Authentication/Evaluation.js
+++ src/pages/Authentication/Evaluation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthWrapper from './Components/AuthWrapper';
 import { Steps } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
@@ -265,27 +265,33 @@ const SignUpForm = ({ handleChange, handleClick, step }) => {
 const EvaluationForm = ({ state, step, next, handleChangeEvaluation }) => {
   const [question, setQuestion] = useState('0a');
 
- 
-  const handleNextAnswer = () => {
-    const value  = state[question];
+  useEffect(() => {
+    const startingPoint = state['0'] ==='Group' ? '16' : state['0'] === 'Family' ? '14' : '0a';
+    setQuestion(startingPoint);
+  }, []);
 
-    console.log('state',state,value)
-    
-    if( questions[question]  ) {
-       const obj = questions[question];
-    const destination = obj.options ?  obj.answerOptions[value] : obj.next;
-    setQuestion(destination)
-    }else{
-      next()
+  const handleNextAnswer = () => {
+    const value = state[question];
+
+    console.log('state', state, value);
+    const obj = questions[question];
+    if (obj.final) {
+      next();
+    } else {
+      const destination = obj.options ? obj.answerOptions[value] : obj.next;
+      setQuestion(destination);
     }
-   
   };
   console.log('e questions[question]', questions[question]);
   if (step !== 1) return null;
   if (state['0'] == 'Company') return <ForCompany />;
   const inputType = {
     input: (
-      <Input placeholder="Fullname" name={question} onChange={handleChangeEvaluation} />
+      <Input
+        placeholder="Fullname"
+        name={question}
+        onChange={handleChangeEvaluation}
+      />
     ),
     select: (
       <Select
@@ -312,7 +318,9 @@ const EvaluationForm = ({ state, step, next, handleChangeEvaluation }) => {
 
       {inputType[questions[question]['type']]}
 
-      <Button className="btn primary btn-sm " onClick={handleNextAnswer}>Submit</Button>
+      <Button className="btn primary btn-sm " onClick={handleNextAnswer}>
+        Submit
+      </Button>
     </div>
   );
 };
@@ -326,13 +334,13 @@ const questions = {
     answerOptions: {
       yes: '0b',
       no: '1',
-    }
+    },
   },
   '0b': {
     name: 'how long have you been married',
     type: 'input',
     options: false,
-    next: '1'
+    next: '1',
   },
   '1': {
     name: 'Gender',
@@ -340,152 +348,333 @@ const questions = {
     type: 'select',
     options: true,
     answerOptions: {
-      'female': '1b',
-      'male': '2',
-    }
+      female: '1b',
+      male: '2',
+    },
   },
   '1b': {
     name: 'Are you planning to have kids in the next 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '1c'
+    next: '1c',
   },
   '1c': {
     name: 'Are you a nursing mother',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '2'
+    next: '2',
   },
   '2': {
     name: 'Have you been to the dentist/optician in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3a'
+    next: '3a',
   },
   '3a': {
     name: 'Have you treated for Asthma in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3b'
+    next: '3b',
   },
   '3b': {
     name: 'Have you treated for Hypertension in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3c'
+    next: '3c',
   },
   '3c': {
     name: 'Have you treated for Ulcer in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3d'
+    next: '3d',
   },
   '3d': {
     name: 'Have you treated for Sickle cell in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3e'
+    next: '3e',
   },
   '3e': {
     name: 'Have you treated for Diabetes in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '3f'
+    next: '3f',
   },
   '3f': {
-    name: 'Have you treated for Epilepsy or seizure disorders in the last 12 months',
+    name:
+      'Have you treated for Epilepsy or seizure disorders in the last 12 months',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '4'
+    next: '4',
   },
   '4': {
     name: 'what is your blood group?',
-    values: ['A', 'B', 'O','AB'],
+    values: ['A', 'B', 'O', 'AB'],
     type: 'select',
     options: false,
-    next: '5'
+    next: '5',
   },
   '5': {
     name: 'what is your blood group?',
     values: ['AA', 'AS', 'SS', 'AC', 'SC'],
     type: 'select',
     options: false,
-    next: '6'
+    next: '6',
   },
   '6': {
     name: 'Are you currently managing any chronic health conditions?',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '7'
+    next: '7',
   },
   '7': {
     name: 'Are you currently on any medications',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '8'
+    next: '8',
   },
   '8': {
     name: 'Do you have any drug allergies?',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '9'
+    next: '9',
   },
   '9': {
     name: 'Have you had any surgery in the past 12 months?',
     values: ['yes', 'no'],
     type: 'select',
-    options: true, 
+    options: true,
     answerOptions: {
-      'yes': '9a',
-      'no': '10',
-    }
+      yes: '9a',
+      no: '10',
+    },
   },
   '9a': {
     name: 'Duration of time spent in the hospital in days',
-    values: ['<5', '6-10', '11-20','>20'],
+    values: ['<5', '6-10', '11-20', '>20'],
     type: 'select',
     options: false,
-    next: '9b'
+    next: '9b',
   },
   '9b': {
     name: 'Any complications from previous surgery',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '9c'
+    next: '9c',
   },
   '9c': {
     name: 'Were symptoms relieved after procedure',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '9d'
+    next: '9d',
   },
   '9d': {
     name: 'Are you still receiving treatment for the procedure',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '10'
+    next: '10',
   },
   '10': {
     name: 'Any known allergies?',
     values: ['yes', 'no'],
     type: 'select',
     options: false,
-    next: '11'
+    next: '11',
+  },
+  '11': {
+    name: 'Have you visited any specialist in the last 12 months',
+    values: ['yes', 'no'],
+    type: 'select',
+    options: true,
+    answerOptions: {
+      yes: '11a',
+      no: '12',
+    }, 
+  },
+  '11a': {
+    name: 'Mention the specialist visited',
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    next: '11',
+  },
+  '12': {
+    name: 'What\'s your average monthly income',
+    values: ['<50k', '51-100K', '101k-250K', '>250K'],
+    type: 'select',
+    options: false,
+    next: '13',
+  },
+  '13': {
+    name: 'How old are you?',
+    values: ['18-24', '25-38', '39-54', '>55'],
+    type: 'select',
+    options: false,
+    final: true, 
+  },
+  '14': {
+    name: 'Marital status',
+    values: ['Married', 'Single'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '15a'
+  },
+  '15a': {
+    name: 'Number of dependents', 
+    type: 'input',
+    options: false,
+    final: false, 
+    next: '15b'
+  },
+  '15b': {
+    name: 'Any dependent/child within the ages 0 -18 yrs',
+    values: ['yes', 'no'],
+    type: 'select',
+    options: true,
+    final: false,  
+    answerOptions: {
+      yes: '15c',
+      no: '0b',
+    }, 
+  },
+  '15c': {
+    name: 'Retrieving data. Wait a few seconds and try to cut or copy again.',
+    type: 'input',
+    options: false,
+    next: '0b',
+  }, 
+  '16': {
+    name: 'How old are you?',
+    values: ['<10', '10-20','21-50','51-100','101-250','251-500','501-750','751-1000', '>1000'],
+    type: 'select',
+    options: false,
+    final: false,
+    next: '17',
+  },
+  '17': {
+    name: 'Number of females', 
+    type: 'input',
+    options: false,
+    final: false, 
+    next: '18'
+  },
+  '18': {
+    name: 'Number of Males', 
+    type: 'input',
+    options: false,
+    final: false, 
+    next: '19a'
+  },
+  '19a': {
+    name: 'Select the care package that best suits your group, Dental care?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19b'
+  },
+  '19b': {
+    name: 'Optical care?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19c'
+  },
+  '19c': {
+    name: 'Prostrate/Cervical care?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19d'
+  },
+  '19d': {
+    name: 'Procedures?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19e'
+  },
+  '19e': {
+    name: 'Diabetes/Asthma/Ulcer/Sickle cell/Seizure disorder?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19f'
+  },
+  '19f': {
+    name: 'Dietician?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19g'
+  },
+  '19g': {
+    name: 'Gym?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19h'
+  },
+  '19h': {
+    name: 'Chronic Treatment?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19i'
+  },
+  '19i': {
+    name: 'Specialist consultation?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19j'
+  },
+  '19j': {
+    name: 'Inpatient care?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19k'
+  },
+  '19k': {
+    name: 'Maternity care?', 
+    values: ['yes', 'no'],
+    type: 'select',
+    options: false,
+    final: false, 
+    next: '19l'
+  },
+  '19l': {
+    name: 'Other interest', 
+    type: 'input',
+    options: false,
+    final: true,
+    next: '1',
   },
 };
 export default Evaluation;
